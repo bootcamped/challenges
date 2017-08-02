cfdb578ff62b44262e977861a937982ee6bebd87
Shawn <shawn@ievolved.io>
2017-08-02 13:11:52 -0700

    [feature] Initial commit with LeapYear.js and Password.js

---
+++ Daily/LeapYear.js
@@ -0,0 +1,23 @@
+
+// Create the function, isLeapYear(year) { ... } the returns the boolean: true/false indicating whether
+//  the given year is a leap year.
+//
+// A year is a valid leap year when:
+//
+//   * Divisible by 4 or 400, but not 100.
+//
+//
+// Use the fewest amount of `if` statements as possible.  No more than 1.  Can you do it with out any
+//  at all?
+//
+//
+// ES6 BONUS:
+//
+//   Convert the function to a generator that accepts multiple years and yields the boolean one at a
+//    time.
+//
+//
+
+
+
+// <your solution here>
---
+++ Daily/Password.js
@@ -0,0 +1,25 @@
+
+
+// No one likes it when others figure out their password.  Business's especially.  But we're
+//  not just any business, we're ACME Unicorn -- and we have a top secret black ops project
+//  that no one can ever know about.  We need to make sure that our coders have really strong
+//  passwords so our rival, BRUTE Co. can't script a viable password cracker and compromise
+//  our systems.
+//
+// We're tasked to create a function that determines whether as password is strong enough.
+//
+// OWASP (www.owasp.org) is a trusted repository of security and vulnerabilities information
+//  that helps educate us how to build secure systems.
+//
+// Visit:
+//
+//   https://www.owasp.org/index.php/Authentication_Cheat_Sheet
+//
+// , to learn about the rules for a strong password.  Using those rules, Write the function
+//  isPasswordStrong(...) that returns the boolean true/false indicating whether the given
+//  password is strong enough for us.
+//
+
+
+
+// <your solution here>
